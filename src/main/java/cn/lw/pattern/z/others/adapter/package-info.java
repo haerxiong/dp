b2a2639a423bb1274f9a301a18b87057738db1aa
haerxiong <haerxiong@gmail.com>
2018-10-18 10:58:50 +0800

    结构型-设计模式中的3种
    组合模式、适配器模式、外观模式

---
+++ src/main/java/cn/lw/pattern/z/others/adapter/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 适配器模式
+ *
+ * 将一个类的接口转换成客户希望的另外一个接口。
+ *
+ * 有动机地修改一个正常运行的系统的接口，这时应该考虑使用适配器模式。
+ *
+ * Created by VictorLiu on 2018/10/18.
+ */
+package cn.lw.pattern.z.others.adapter;
---
+++ src/main/java/cn/lw/pattern/z/others/facade/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 外观模式
+ *
+ * 使用场景： 1、为复杂的模块或子系统提供外界访问的模块。 2、子系统相对独立。 3、预防低水平人员带来的风险。
+ *
+ * Created by VictorLiu on 2018/10/18.
+ */
+package cn.lw.pattern.z.others.facade;
